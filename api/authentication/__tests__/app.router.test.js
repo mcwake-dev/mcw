52955050f8278cbeafaf15444ad2b4e8fdde013d
Matthew C Wake <mcwake-dev@outlook.com>
2022-05-13 20:16:50 +0100

    Added tests for username and email existence check

--- api/authentication/__tests__/app.router.test.js
+++ api/authentication/__tests__/app.router.test.js
@@ -11,4 +11,38 @@ describe("Authentication", () => {
       expect(response.body).toHaveProperty("public_key");
     });
   });
+  describe("GET /api/authentication/exists/username/:username - user exists", () => {
+    it("should return 200 when user exists", async () => {
+      const response = await request(app).get(
+        "/api/authentication/exists/username/:username"
+      );
+
+      expect(response.statusCode).toBe(200);
+    });
+    it("should return 404 when user does not exist", async () => {
+      const response = await request(app).get(
+        "/api/authentication/exists/username/:username"
+      );
+
+      expect(response.statusCode).toBe(404);
+      expect(response.body).toHaveProperty("error");
+    });
+  });
+  describe("GET /api/authentication/exists/email/:email - email exists", () => {
+    it("should return 200 when email exists", async () => {
+      const response = await request(app).get(
+        "/api/authentication/exists/email/test-user@email.com"
+      );
+
+      expect(response.statusCode).toBe(200);
+    });
+    it("should return 404 when does not email exist", async () => {
+      const response = await request(app).get(
+        "/api/authentication/exists/email/test-user@email.com"
+      );
+
+      expect(response.statusCode).toBe(404);
+      expect(response.body).toHaveProperty("error");
+    });
+  });
 });
