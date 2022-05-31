982b9b2163bab088fec41e6cf0195ab293281be1
Matthew C Wake <mcwake-dev@outlook.com>
2022-05-31 16:13:26 +0100

    Connected router to new functions

--- api/authentication/router/app.router.js
+++ api/authentication/router/app.router.js
@@ -3,35 +3,26 @@ const { celebrate, Segments, errors, Joi } = require("celebrate");
 
 const router = express.Router();
 
-const {
-  emailExists,
-  usernameExists,
-  register,
-} = require("../controller/user.controller");
-const {
-  usernameSchema,
-  emailSchema,
-  userSchema,
-} = require("@mcw/validation/user.validation");
+const { emailInUse, register } = require("../controller/user.controller");
+
+const { emailSchema, userSchema } = require("@mcw/validation/user.validation");
 
 router.get("/api/authentication/pk", (_request, response) => {
   response.status(200).send({ public_key: process.env.JWT_PUBLIC_KEY });
 });
+
 router.get(
-  "/api/authentication/available/username/:username",
-  celebrate({ [Segments.PARAMS]: Joi.object({ username: usernameSchema }) }),
-  usernameExists
-);
-router.get(
-  "/api/authentication/available/email/:email",
+  "/api/authentication/in-use/email/:email",
   celebrate({ [Segments.PARAMS]: Joi.object({ email: emailSchema }) }),
-  emailExists
+  emailInUse
 );
+
 router.post(
   "/api/authentication/register",
   celebrate({ [Segments.BODY]: userSchema }),
   register
 );
+
 router.use(errors());
 
 module.exports = router;
