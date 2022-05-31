012e3bfd554e722497db93ff015e9807c209c995
Matthew C Wake <mcwake-dev@outlook.com>
2022-05-31 16:18:41 +0100

    Tidied up app layout

--- web/authentication/pages/_app.js
+++ web/authentication/pages/_app.js
@@ -2,41 +2,32 @@ import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div id="container">
-      <div className="row">
-        <h1 className="text-lightest bold">Hi!</h1>
-        <h2 className="text-dark bold">I'm Matthew</h2>
-        <h3 className="text-mid-dark bold">And I write</h3>
-        <h3 className="text-lightest bold">
-          <span className="text-lighter">&lt;code /&gt;</span> for the Web
-        </h3>
-      </div>
-      <div className="row list">
+    <div id="container" className="bg-dark">
+      <nav id="menu">
         <ul>
           <li>
-            <a href="https://github.com/mcwake-dev" target="_blank">
+            <a id="brand" className="text-yellow p-05" href="/">
+              MCW
+            </a>
+          </li>
+          <li>
+            <a
+              className="text-grey border-grey p-05"
+              href="https://github.com/mcwake-dev"
+              target="_blank"
+            >
               <i className="fa fa-github"></i> Github
             </a>
           </li>
           <li>
-            <a href="/blog">
+            <a className="text-grey border-grey p-05" href="/blog">
               <i className="fa fa-bullhorn"></i> Blog
             </a>
           </li>
-          <li>
-            <a href="#register">
-              <i className="fa fa-cut-key"></i> Register
-            </a>
-          </li>
-          <li>
-            <a href="#login">
-              <i className="fa fa-sign-in"></i> Login
-            </a>
-          </li>
         </ul>
-      </div>
-      <Component {...pageProps} />
-      <footer className="footer">
+      </nav>
+      <Component id="content" {...pageProps} />
+      <footer id="footer" className="text-grey">
         All Code, Text and Images &copy; Matthew C Wake 2022
       </footer>
     </div>
--- web/authentication/pages/_document.js
+++ web/authentication/pages/_document.js
@@ -11,7 +11,11 @@ export default function Document() {
           crossOrigin="anonymous"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:wght@400;900&display=swap"
           rel="stylesheet"
