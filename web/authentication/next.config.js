b2d3e8273737b724c053f24997e51c4dc3b24d0a
Matthew C Wake <mcwake-dev@outlook.com>
2022-06-10 00:59:04 +0100

    Added proxy for api calls to hide server URL

--- web/authentication/next.config.js
+++ web/authentication/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const API_SERVER = "http://localhost:8000";
 
-module.exports = nextConfig;
+module.exports = {
+  reactStrictMode: true,
+  rewrites: async function () {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${API_SERVER}/api/:path*`,
+      },
+    ];
+  },
+};
